e1cf377a06afa372ad0bf926041ec841f76526e8
Jan <stoltmanjan@gmail.com>
2018-09-08 13:01:01 +0200

    Library found

--- app/src/main/java/com/yggdralisk/myapplication/MainActivity.kt
+++ app/src/main/java/com/yggdralisk/myapplication/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.yggdralisk.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var scannerDisposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        //TODO: add permisison check
+
+        scannerDisposable = barcodeView
+                .getObservable()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    result.text = it.rawValue
+                }, {
+                })
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        scannerDisposable?.dispose()
     }
 }
